809bf86c9587a6d144c991ba199477af1a6ea97e
Bruno Rodrigues <flyingdutchmanltda@gmail.com>
2023-10-24 15:21:22 -0300

    Commit Inicial

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:stockflow/telas/cadastrar/widget_principal/cadastrar.dart';
+
+void main(){
+  runApp(const MaterialApp(
+  home: Cadastrar(),
+  ));
+}
